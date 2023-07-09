a36510ed8b99adfc5e7ec713a4799f87c27adfe1
ggnaz <nazersomera123@gmail.com>
2023-07-09 23:04:41 +0800

    saved

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { useField, Form, FormikProps, Formik } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  // contactNumber: Yup.string().test({
+  //   name: "is-sku",
+  //   skipAbsent: true,
+  //   test(value, ctx) {
+  //     if (value.startsWith("09"))
+  //       return ctx.createError({ message: "SKU missing correct prefix" });
+  //   },
+  // }),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+const MyTextField = ({ label, ...props }: any) => {
+  const [field, meta, helpers] = useField(props);
+  return (
+    <>
+      <label>
+        {label}
+        <input {...field} {...props} />
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
 export default function page() {
+  const sampleObj = {
+    email: "nana@mail.com",
+    firstName: "red",
+    lastName: "rose",
+  };
+
   return (
-    <div>dashboard</div>
-  )
+    <div>
+      <h1>My Form</h1>
+      <Formik
+        initialValues={sampleObj}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {(props: FormikProps<Values>) => (
+          <Form>
+            <MyTextField name="firstName" type="text" label="First Name" />
+            <MyTextField name="lastName" type="text" label="Last Name" />
+            <MyTextField name="email" type="email" label="Email" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import React, { useState } from "react";
+import axios from "axios";
+import useSWR from "swr";
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default function Home() {
   const router = useRouter();
@@ -9,11 +12,18 @@ export default function Home() {
   const isClick = (clicked) => {
     if (clicked) return router.push("/admin/dashboard");
   };
+  const { data, error, isLoading } = useSWR(
+    "https://fakestoreapi.com/products/1",
+    fetcher
+  );
+  console.log("data", data);
+  console.log("error", error);
   return (
     <div className="flex w-full h-screen">
-      <button className="text-white" onClick={() => isClick(true)}>
-        Login
+      <button className="" onClick={() => isClick(true)}>
+        Login {isLoading}
       </button>
+      <div className=" flex flex-col">{JSON.stringify(data, "\t")}</div>
     </div>
   );
 }
